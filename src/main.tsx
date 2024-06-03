2cde22600198e834605ae6371923f64ca7185a84
DeveloperYamin <web.developer.yamin@gmail.com>
2024-06-03 23:01:26 +0600

    fix: style setup issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/* This code snippet is setting up a basic React application using TypeScript. Here's a breakdown of
+what each part of the code is doing: */
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
+import { theme } from "./themes/materialUi";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.headers.common["Content-Type"] = "application/json";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <App />
+          <Toaster position="top-center" reverseOrder={true} />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
